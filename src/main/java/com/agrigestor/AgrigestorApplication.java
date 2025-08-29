6ddb3e46f745bf0aad59e8e5cad90963c0e1fca4
alitakallyne <alytakallyne@gmail.com>
2025-08-29 19:42:18 -0300

    🚀 Projeto inicial AgriGestor - Spring Boot 3 + Java 17

---
+++ src/main/java/com/agrigestor/AgrigestorApplication.java
@@ -0,0 +1,13 @@
+package com.agrigestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrigestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrigestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrigestor/AgrigestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrigestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrigestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
